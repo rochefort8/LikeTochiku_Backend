249819742b7c2fda37e86b3c55432a2bcca19ad5
Yuji Ogihara <yuji.ogihara.85@gmail.com>
2015-04-13 23:29:56 +0900

    Add form for tochikunn-quiz

---
+++ q/confirm.php
@@ -0,0 +1,92 @@
+<?php
+
+$beerName = isset($_POST["beerName"])? $_POST["beerName"] : "";
+$beerName_JP = isset($_POST["beerName_JP"])? $_POST["beerName_JP"] : "";
+$beerType = isset($_POST["beerType"])? $_POST["beerType"] : "";
+$beerDescription = isset($_POST["beerDescription"])? $_POST["beerDescription"] : "";
+$beerImage = isset($_FILES["beerImage"]["name"])? $_FILES["beerImage"]["name"] : "" ; 
+$beerImage_uploaded_path = "/tmp/" . $_FILES["beerImage"]["name"] ;
+
+if (is_uploaded_file($_FILES["beerImage"]["tmp_name"])) {
+  if (move_uploaded_file($_FILES["beerImage"]["tmp_name"], "/tmp/" . $_FILES["beerImage"]["name"])) {
+    chmod("/tmp/" . $_FILES["beerImage"]["name"], 0644);
+  } else {
+  }
+} else {
+
+}
+
+// ----- 入力チェック -----
+$err_msg = array();	// エラーメッセージを格納する配列
+
+// ----- 画面表示 -----
+require_once("header.php");
+?>
+<body>
+<div class="main">
+<div id="contactInfo">
+
+	<h1>お問い合わせ（確認）</h1>
+
+	<?php if(count($err_msg) > 0){?>
+	<div class="error">
+		入力エラーがあります。<br>
+		<ul>
+			<?php foreach($err_msg as $msg){?>
+				<li><?php echo $msg;?></li>
+			<?php }?>
+		</ul>
+	</div>
+	<?php }else{ ?>
+		<p>
+		この内容でよろしければ、「送信」をクリックしてください。
+		<br>メールアドレスに間違いがあると回答を返信できませんので十分ご確認ください。
+		</p>
+	<?php }?>
+
+	<form name="form1" action="./send.php" method="post">
+	  <input type="hidden" name="beerName" value="<?php echo htmlspecialchars($beerName, ENT_QUOTES, "UTF-8");?>">
+	  <input type="hidden" name="beerName_JP" value="<?php echo htmlspecialchars($beerName_JP, ENT_QUOTES, "UTF-8");?>">
+	  <input type="hidden" name="beerType" value="<?php echo htmlspecialchars($beerType, ENT_QUOTES, "UTF-8");?>">
+	  <input type="hidden" name="beerDescription" value="<?php echo htmlspecialchars($beerDescription, ENT_QUOTES, "UTF-8");?>">
+
+	  <input type="hidden" name="beerImage" value="<?php echo htmlspecialchars($beerImage, ENT_QUOTES, "UTF-8");?>">
+	  <input type="hidden" name="beerImage_uploaded_path" value="<?php echo htmlspecialchars($beerImage_uploaded_path, ENT_QUOTES, "UTF-8");?>">
+
+	  <table  class="contact_tbl">
+	    <tr>
+	      <th>名称(ローマ字）</th>
+	      <td>
+	        <?php echo htmlspecialchars($beerName, ENT_QUOTES, "UTF-8");?>
+	      </td>
+	    </tr>
+	    <tr>
+	      <th>名称(日本語）</th>
+	      <td><?php echo htmlspecialchars($beerName_JP, ENT_QUOTES, "UTF-8");?></td>
+	    </tr>
+	    <tr>
+	      <th>種類</th>
+	      <td><?php echo nl2br(htmlspecialchars($beerType, ENT_QUOTES, "UTF-8"));?></td>
+	    </tr>
+	    <tr>
+	      <th>説明</th>
+	      <td><?php echo nl2br(htmlspecialchars($beerDescription, ENT_QUOTES, "UTF-8"));?></td>	    </tr>
+	    <tr>
+	      <th>画像</th>
+	      <td><?php echo nl2br(htmlspecialchars($beerImage, ENT_QUOTES, "UTF-8"));?></td>
+	    </tr>
+	  </table>
+	  <div id="btn_area">
+		  <input type="button" name="back_btn" value="戻る"
+		         onclick="form1.action='./index.php';form1.submit();" class="btn">
+		  &nbsp;&nbsp;
+		  <?php if(count($err_msg) == 0){?>
+		  <input type="submit" name="next_btn" value="送信" class="btn">
+		  <?php }?>
+	  </div>
+	</form>
+
+</div><!--/contactInfo-->
+</div><!--/main-->
+</body>
+</html>
---
+++ q/send.php
@@ -0,0 +1,151 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>sample</title>
+</head>
+<body>
+
+
+<?php
+
+
+// POSTされたデータを取得します。
+$beerName = isset($_POST["beerName"])? $_POST["beerName"] : "";
+$beerName_JP = isset($_POST["beerName_JP"])? $_POST["beerName_JP"] : "";
+$beerType = isset($_POST["beerType"])? $_POST["beerType"] : "";
+$beerDescription = isset($_POST["beerDescription"])? $_POST["beerDescription"] : "";
+$beerImage = isset($_POST["beerImage"])? $_POST["beerImage"] : "";
+$beerImage_uploaded_path = isset($_POST["beerImage_uploaded_path"])? $_POST["beerImage_uploaded_path"] : "";
+
+function generate_file_basename($str)
+{
+	$_str=str_replace(" ","_",$str); 
+	return $_str ;
+}
+
+function create_xml($name_en,$name_jp,$type,$description,$image,$saveto)
+{
+	$dom = new DomDocument('1.0', 'UTF-8');
+
+	$prefs = $dom->appendChild($dom->createElement('belgianbeer'));
+	$pref = $prefs->appendChild($dom->createElement('beer'));
+
+	$pref->setAttribute('name', $name_en);
+	$pref->appendChild($dom->createElement('name_en', $name_en));
+	$pref->appendChild($dom->createElement('name_jp', $name_jp));
+	$pref->appendChild($dom->createElement('type', $type));
+	$pref->appendChild($dom->createElement('description', $description));
+	$pref->appendChild($dom->createElement('image', $image));
+
+	$dom->formatOutput = true;
+	$dom->save($saveto) ;
+}
+
+function create_zip_archive($dirname)
+{
+	$files = array_diff( scandir($dirname), array(".", "..") );
+
+	$zip = new ZipArchive();
+	$res = $zip->open($dirname . ".zip", ZipArchive::CREATE);
+ 	
+	if($res === true){
+	     foreach($files as $file){
+	            $zip->addFile($dirname . "/" . $file);
+	     }
+    	     $zip->close();
+	} else {
+	    echo 'Error Code: ' . $res;
+	}
+}
+
+require 'parse-php-sdk/autoload.php';
+
+date_default_timezone_set('Asia/Tokyo');
+
+use Parse\ParseClient;
+use Parse\ParseObject;
+use Parse\ParseFile;
+
+ParseClient::initialize(
+	'rLfiUPlbIE5orN0Al07gpotnvIpqwTUpoQlkhjO0',
+	'LnIgqdYSz8krs6iKBdH5XtGqglkyjzuSEHTnNbEC', 
+	'jtNDkVGTpaVeregAuvlTYOUCErbKnSMgE7F6x9Fo'
+	);
+
+function upload_to_parse( $name, $image, $description, $archive )
+{
+    $obj = ParseObject::create("BelgianBeer");
+
+    $file_image       = ParseFile::createFromFile( $image ,basename($image)) ;
+    $file_description = ParseFile::createFromFile( $description ,basename($description)) ;
+    $file_archive     = ParseFile::createFromFile( $archive ,basename($archive)) ;
+
+    $obj->set( "beerName" , $name ) ;
+    $obj->set( "beerImage" , $file_image ) ;
+    $obj->set( "beerDescription" , $file_description ) ;
+    $obj->set( "archive" , $file_archive ) ;
+
+    $obj->save() ;
+}
+
+$base_filename = generate_file_basename($beerName) ;
+
+$tmp_dirname = "/tmp/" . $base_filename ;
+$dirname = $base_filename ;
+
+//rmdir ( $tmp_dirname ) ;
+//system ( "rm -rf" . " " . $tmp_dirname ) ;
+mkdir ( $tmp_dirname ) ;
+rename ($beerImage_uploaded_path, $tmp_dirname . "/" . $base_filename . ".jpg") ;
+
+create_xml( $beerName, $beerName_JP, $beerType,
+	    $base_filename . ".txt", $base_filename . ".jpg",
+	    $tmp_dirname . "/" . "beer.xml" ) ;
+file_put_contents( $tmp_dirname . "/" . $base_filename . ".txt" , $beerDescription) ;
+chdir('/tmp/') ;
+create_zip_archive($dirname) ;
+
+$image = $tmp_dirname . "/" . $base_filename . ".jpg" ;
+$description = $tmp_dirname . "/" . $base_filename . ".txt" ;
+$archive = "/tmp/" . $base_filename . ".zip" ;
+
+upload_to_parse( $beerName, $image, $description, $archive ) ;
+
+$upload_success = true ;
+
+require_once("header.php");
+?>
+
+<body>
+<div class="main">
+<div id="contactInfo">
+
+<?php if($upload_success){ ?>
+	<h1>お問い合わせ（完了）</h1>
+	<div id="thanks">
+		<p>
+			お問い合わせを送信しました。<br>
+			後日担当者から、ご連絡を差し上げます。今しばらくお待ちくださいませ。<br>
+			<br>
+			※日程によりましては、お返事に時間がかかる場合がございます。何卒ご了承くださいませ。
+		</p>
+	</div>
+<?php } ?>
+
+<?php if(!$upload_success){ ?>
+	<h1>お問い合わせ（送信失敗）</h1>
+	<p>
+		<span class="error">送信に失敗しました。</span>
+	</p>
+<?php } ?>
+	  <div id="btn_area">
+		  <input type="button" name="back_btn" value="戻る"
+		         onclick="location.href='./index.php'"" class="btn">
+
+	  </div>
+
+</div><!--/contactInfo-->
+</div><!--/main-->
+
+</body>
+</html>
